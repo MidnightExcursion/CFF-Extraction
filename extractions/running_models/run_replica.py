ed3f850bbd310c89df8c9aaf2bac7d2c88b060ac
ErenMcLaren <dima.watkins@gmail.com>
2024-05-22 11:00:50 -0400

    Finished up the splitting of the ANN data.

--- extractions/running_models/run_replica.py
+++ extractions/running_models/run_replica.py
@@ -2,9 +2,28 @@ from pandas import DataFrame
 
 from utilities.generation.generate_pseudodata import generate_replica_data
 
+from statics.strings.static_strings import _COLUMN_NAME_X_BJORKEN
+from statics.strings.static_strings import _COLUMN_NAME_Q_SQUARED
+from statics.strings.static_strings import _COLUMN_NAME_T_MOMENTUM_CHANGE
+from statics.strings.static_strings import _COLUMN_NAME_AZIMUTHAL_PHI
+from statics.strings.static_strings import _COLUMN_NAME_LEPTON_MOMENTUM 
+from statics.strings.static_strings import _COLUMN_NAME_CROSS_SECTION
+from statics.strings.static_strings import _COLUMN_NAME_CROSS_SECTION_ERROR
+
 def run_DNN_replica(kinematic_set, replica_number: int, base_dataframe: DataFrame):
 
     # (1): Generate the data for this DNN replica:
     generated_replica_data = generate_replica_data(base_dataframe)
+
+    # (2): Identify the "x values" for our model:
+    x_data = generated_replica_data[[_COLUMN_NAME_Q_SQUARED, _COLUMN_NAME_X_BJORKEN, _COLUMN_NAME_T_MOMENTUM_CHANGE, _COLUMN_NAME_AZIMUTHAL_PHI, _COLUMN_NAME_LEPTON_MOMENTUM]]
+
+    # (3): Identify the "y values" for our model:
+    y_data = generated_replica_data[_COLUMN_NAME_CROSS_SECTION]
+    y_data_error = generated_replica_data[_COLUMN_NAME_CROSS_SECTION_ERROR]
     
-    # (2): 
+    # (3): Split the Data into Training/Testing:
+    train_x_data, training_y_data, training_y_error_data, testing_x_data, testing_y_data, testing_y_error_data = split_data()
+
+    # (4): Initialize the DNN:
+    neural_network = 0.
