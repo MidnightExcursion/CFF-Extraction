6f559777bf36f0810e23158cf9095a7d08ab7e80
ErenMcLaren <dima.watkins@gmail.com>
2024-05-27 10:34:22 -0400

    All we need to do is just replace the F-values in the DF. Goddamn it!

--- utilities/generation/generate_pseudodata.py
+++ utilities/generation/generate_pseudodata.py
@@ -11,7 +11,7 @@ def generate_replica_data(dataframe: DataFrame):
     try:
 
         # (1): Reaplce the Cross Section column with a number sampled from a Normal Distribution:
-        dataframe[_COLUMN_NAME_CROSS_SECTION] = [sample_from_numpy_normal_distribution(*cross_section_and_error_tuple) for cross_section_and_error_tuple in tuple(zip(dataframe[_COLUMN_NAME_CROSS_SECTION], dataframe[_COLUMN_NAME_CROSS_SECTION_ERROR]))]
+        dataframe.loc[_COLUMN_NAME_CROSS_SECTION] = [sample_from_numpy_normal_distribution(*cross_section_and_error_tuple) for cross_section_and_error_tuple in tuple(zip(dataframe[_COLUMN_NAME_CROSS_SECTION], dataframe[_COLUMN_NAME_CROSS_SECTION_ERROR]))]
 
         # (2): Let's just set these things to be the same for clarity:
         replica_dataframe = dataframe
