fa80a374e60afb2f7ae1f88432ce2b44497aac44
ErenMcLaren <dima.watkins@gmail.com>
2024-06-03 13:21:22 -0400

    Finally fixed the analytics.py file so at least *something* comes out of it.

--- extractions/running_models/analytics.py
+++ extractions/running_models/analytics.py
@@ -3,6 +3,7 @@ from numpy import array
 
 # Native Library | TensorFlow
 import tensorflow as tf
+from tensorflow.keras.models import Model
 
 from statics.strings.static_strings import _COLUMN_NAME_X_BJORKEN
 from statics.strings.static_strings import _COLUMN_NAME_Q_SQUARED
@@ -28,26 +29,19 @@ def perform_replica_analytics(entire_dataframe, trained_neural_network):
     """
     
     # (1): Obtain the kinematics of the entire dataframe:
-    print(entire_dataframe[[_COLUMN_NAME_Q_SQUARED, _COLUMN_NAME_X_BJORKEN, _COLUMN_NAME_T_MOMENTUM_CHANGE, _COLUMN_NAME_AZIMUTHAL_PHI, _COLUMN_NAME_LEPTON_MOMENTUM]])
     all_dataframe_kinematics = entire_dataframe[[_COLUMN_NAME_Q_SQUARED, _COLUMN_NAME_X_BJORKEN, _COLUMN_NAME_T_MOMENTUM_CHANGE, _COLUMN_NAME_AZIMUTHAL_PHI, _COLUMN_NAME_LEPTON_MOMENTUM]]
     
     # (2): Define TensorFlow functions for predicting CFFs:
     @tf.function
     def predict_cffs(input_data):
-        print('fuck')
-        print(trained_neural_network.get_layer(name='cff_output_layer'))
-        print('33')
-        print(trained_neural_network.get_layer(name='cff_output_layer').output)
-        print('245')
-        print(input_data)
-        print('2354235234')
-        return trained_neural_network.get_layer(name='cff_output_layer').output(input_data)
+        output_layer = trained_neural_network.get_layer(name='cff_output_layer').output
+        intermediate_model = Model(inputs = trained_neural_network.input, outputs = output_layer)
+        return intermediate_model(input_data)
     
     # (3): Define TensorFlow functions for predicting the total cross-section:
     @tf.function
     def predict_cross_sections(input_data):
-        print('fuczzzk')
-        return trained_neural_network.get_layer(name='TotalFLayer').output(input_data)
+        return trained_neural_network(input_data)
     
     # (4): Plug-and-chug the DF numbers into the trained DNN to get the CFFs!
     predicted_cffs = array(predict_cffs(all_dataframe_kinematics))
